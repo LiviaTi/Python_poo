dcabcca2e2a9ec25b00fda7a449b49e1e4f2978f
Livia Ferreira Dos Santos <liferrei@c2r8p5.42sp.org.br>
2025-10-22 12:09:56 -0300

    @classmethod, @staticmethod

--- decoradores_comuns.py
+++ decoradores_comuns.py
@@ -10,15 +10,42 @@ class MinhaClasse:
 		self.nome = nome
 	
 	def metodo_instancia(self):
-		return (f"Método de instância chamado para {self.valor}")
+		return (f"Método de instância (self), Ações do objeto, chamado para {self.nome}")
 	
 	@classmethod
-	def metodo_classe(cls):
-		return (f"Método da classe chamado para valor = {cls.valor}")
-
+	def metodo_classe(cls): #cls recebe a clase
+		return (f"Método da classe chamado para valor = {cls.valor}. cls, Criar instâncias, acessar/alterar a classe")
+	
+	@staticmethod
+	def metodo_estatico():
+		return "Método estático: Funções utilitárias relacionadas à classe"
 
 obj = MinhaClasse(nome="Classe exemplo")
 print(obj.metodo_instancia())
 print(MinhaClasse.valor)
 print(MinhaClasse.metodo_classe())
+print(MinhaClasse.metodo_estatico())
 
+class Carro:
+	def __init__(self, marca, modelo, ano)-> None:
+		self.marca = marca
+		self.modelo = modelo
+		self.ano = ano
+	
+	@classmethod
+	def criar_carro(cls, configuracao):
+		marca, modelo, ano = configuracao.split(",")
+		return cls(marca, modelo, int(ano))
+
+configuracao1 = "Toyota,Corolla,2022"
+carro1 = Carro.criar_carro(configuracao1)
+print(f"\n Marca: {carro1.marca} \n Modelo: {carro1.modelo} \n Ano: {carro1.ano}\n ")
+
+# @staticmethod é muito utilizado em bibliotecas
+
+class	Matematica:
+	@staticmethod
+	def somar(a, b):
+		return a + b
+
+print(Matematica.somar(a=10, b=15))
